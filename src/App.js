df221d17d4a3bae913f315f14742f1716934ecb4
domobritton <domonicbritton@gmail.com>
2019-02-16 23:26:34 -0800

    Setup app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: `https://api-uswest.graphcms.com/v1/cjs8kc06b9agq01gjxanpv3m4/master`
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
