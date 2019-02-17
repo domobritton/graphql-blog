ad53a79c57fdf77a15171ac60856716e3c063646
domobritton <domonicbritton@gmail.com>
2019-02-16 23:58:33 -0800

    setup graphcms and make first query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
   uri: `https://api-uswest.graphcms.com/v1/cjs8kc06b9agq01gjxanpv3m4/master`
 });
+
+const POSTS_QUERY = gql`
+  query allPosts {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
+// test the connection to graphcms
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res));
 
 class App extends Component {
   render() {
@@ -15,18 +31,14 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
+          <Query query={POSTS_QUERY}>
+            {({ data, loading }) => {
+              if (loading) return `Loading...`;
+              const { posts } = data;
+              return posts.map(post => <h1 key={post.id}>{post.title}</h1>)
+            }}
+          </Query>
         </div>
       </ApolloProvider>
     );
