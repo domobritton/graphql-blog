5133ce32d4dfd7ba61617317429b7713f3d5f76c
domobritton <domonicbritton@gmail.com>
2019-02-17 21:58:13 -0800

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider, Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import Posts from './Posts/Posts';
+import Post from './Posts/Post';
 import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
   uri: `https://api-uswest.graphcms.com/v1/cjs8kc06b9agq01gjxanpv3m4/master`
 });
-
-const POSTS_QUERY = gql`
-  query allPosts {
-    posts {
-      id
-      title
-      body
-    }
-  }
-`;
 
 // test the connection to graphcms
 // client.query({
@@ -28,18 +20,18 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
+      <Router>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
           </header>
-          <Query query={POSTS_QUERY}>
-            {({ data, loading }) => {
-              if (loading) return `Loading...`;
-              const { posts } = data;
-              return posts.map(post => <h1 key={post.id}>{post.title}</h1>)
-            }}
-          </Query>
+          <Switch>
+            <Route exact path="/" component={Posts} />
+            <Route path="/post/:id" component={Post} />
+          </Switch>
         </div>
+
+      </Router>
       </ApolloProvider>
     );
   }
