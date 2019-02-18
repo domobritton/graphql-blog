612a380b3204d9f7b729bf7b16b01c5d9e57126e
domobritton <domonicbritton@gmail.com>
2019-02-17 22:12:45 -0800

    Add Posts and Post components

--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 export default class Post extends Component {
   render() {
+      const { match } = this.props;
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
+        <Query 
+            query={POST_QUERY} 
+            variables={{
+                id: match.params.id
+            }}
+        >
+            {({ data, loading }) => {
+                if (loading) return `Loading...`;
+                const { post } = data;
+                return <h1 key={post.id}>{post.title}</h1>
+            }}
+        </Query>
     )
   }
 }
+const POST_QUERY = gql`
+  query post($id: ID!) {
+    post(where: { id: $id }) {
+        id
+        title
+        body 
+    }
+  }
+`;
